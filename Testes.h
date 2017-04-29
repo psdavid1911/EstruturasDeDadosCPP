2c27e2ab233c949b11e10b5f6e916346f92365d7
david <david@localhost>
2017-04-29 12:06:07 -0300

    Em edição permanente

--- Testes.h
+++ Testes.h
@@ -1,13 +1,4 @@
 #pragma once
 #include "bibliotecas.h"
-#include "classeListaEncadeada.h"
 
-// DEU CERTO!!!!
-
-void Testes(){
-//    ListaEncadeada<int> lista; // cuidado ele se confunde quando não existem construtores com parâmettros
-//    lista.adicionaAoFinal(1);
-//    lista.adicionaAoFinal(2);
-//    cout << lista.pega() << endl;
-//    lista.imprimeStruct();
-}
+void Testes(){}
